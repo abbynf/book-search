07127f4b245b8af19282961bf3278f9d45676508
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-02 13:09:28 -0700

    created schema for books

---
+++ server/models/book.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    // set up properties for new book schema
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+});
+
+const Book = mongoose.model('Book', BookSchema);
+
+module.exports = Book;
