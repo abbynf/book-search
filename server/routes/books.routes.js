dae45e3ae6b128484dc573e32861567f22e20a01
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-02 12:48:25 -0700

    added test routes for post and delete

--- server/routes/books.routes.js
+++ server/routes/books.routes.js
@@ -3,8 +3,23 @@ const router = require('express').Router();
 router
     .route('/')
     .get((req, res) => {
+        // return all saved books as JSON
         console.log('hit the get route in /api/books');
         res.json({ success: true})
+    })
+    .post((req, res) => {
+        // save new book to the database
+        console.log("post route hit");
+        console.log({reqBody: req.body})
+        res.json({ success: true})
     });
+router
+    .route('/:id')
+    .delete((req, res) => {
+        console.log("delete route hit");
+        const reqDel =  req.params.id
+        console.log("params", reqDel);
+        res.json({ success: true})
+    })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
