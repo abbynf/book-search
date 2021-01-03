5b20165583235977715c2dd3d582f844520f62d1
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-02 19:15:34 -0700

    add post route to add books to db

--- server/routes/books.routes.js
+++ server/routes/books.routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Book } = require('../models');
 
 router
     .route('/')
@@ -11,17 +12,34 @@ router
         // save new book to the database
         console.log("post route hit");
         console.log({reqBody: req.body})
-        res.json({ success: true})
+
+        Book
+            .create({
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                image: req.body.image,
+                link: req.body.link
+            })
+            .then(data => {
+                console.log({ data });
+                res.json({ success: true, data })
+            })
+            .catch(error => {
+                console.log('error adding to db', {error})
+            })
+
+
     });
-router
-    .route('/:id')
-    .delete((req, res) => {
-        // Delte book from the database
-        console.log("delete route hit");
-        const reqDel =  req.params.id
-        console.log("params", reqDel);
-        res.json({ success: true})
-    })
+// router
+//     .route('/:id')
+//     .delete((req, res) => {
+//         // Delte book from the database
+//         console.log("delete route hit");
+//         const reqDel =  req.params.id
+//         console.log("params", reqDel);
+//         res.json({ success: true})
+//     })
 // * route
 
 module.exports = router;
