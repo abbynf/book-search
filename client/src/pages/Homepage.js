affe31223501bb2fd2a766fff4da8b9224e84e90
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-05 15:09:53 -0700

    created axios post request

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -37,6 +37,9 @@ class Homepage extends Component {
     handleSaveClick = (event) => {
         event.preventDefault();
         const chosenVolume = this.state.searchResults.filter(result => result.id === event.target.value);
+        console.log(chosenVolume);
+        API.saveTitle(chosenVolume)
+        .then(res => console.log(res))
     }
 
 
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import Format from './../utils/Format';
 
+// eslint-disable-next-line
 export default {
     searchTitle: function(title) {
         const url = "https://www.googleapis.com/books/v1/volumes?q=" + title;
         return axios.get(url)
+    },
+    saveTitle: function(volume) {
+        const info = volume[0].volumeInfo;
+        const rightLink = Format(volume[0].id, info.title)
+        return axios.post('/api/books', {
+            title: info.title,
+            author: info.authors[0],
+            description: info.description,
+            image: info.imageLinks.thumbnail,
+            link: rightLink
+        })
     }
 };
