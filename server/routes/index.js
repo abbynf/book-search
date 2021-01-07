04e56b49152f124d77becb1c7f0690f72f781265
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-06 19:52:45 -0700

    added catch all route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const books = require('./books.routes');
+const path = require('path')
 
 router.use('/api/books', books);
+router
+    .route('*')
+    .get((req, res) => {
+        res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    })
 
 module.exports = router;
