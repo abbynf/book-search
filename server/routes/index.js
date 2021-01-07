cffd4d332a68cada049ef7657642db8cb7d1e879
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2021-01-06 21:14:17 -0700

    deploy to heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,5 @@ const books = require('./books.routes');
 const path = require('path')
 
 router.use('/api/books', books);
-router
-    .route('*')
-    .get((req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    })
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 require('./config/db')();
+const path = require('path')
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(routes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"))
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log('app running on PORT: ' + PORT)
